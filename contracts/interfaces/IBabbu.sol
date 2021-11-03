39b66e5258ecb166e896985e69d061b0c7a92ad8
Babbu Protocal <93649856+babbuprotocal@users.noreply.github.com>
2021-11-03 17:48:22 +0700

    Delete contracts directory

--- contracts/interfaces/IBabbu.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity >=0.6.2;
-
-interface IBabbu {
-
-      function isExcludedFromFee(address account) external view returns(bool);
-
-      function owner() external view returns(address);
-}
